ea995f9254c23048962c768185929b5979c8efc6
Lionel scheidt <scheidt.lionel@gmail.com>
2026-02-10 23:02:14 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-318476913-828b6",
+  "appId": "1:665576866769:web:0588f0c7b0dddb060016f0",
+  "apiKey": "AIzaSyCvatLGIkII0hXnMlbqNSQ_9FBoTGmeAA0",
+  "authDomain": "studio-318476913-828b6.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "665576866769"
+};
